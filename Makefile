247578e848d2de4601201e38a23ba72dee60e46d
Minsi Hu <101283845+Mimsqueeze@users.noreply.github.com>
2024-06-22 21:36:33 -0400

    added files

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	g++ -I.\include -L.\lib -o src/main.exe src/main.cpp -lSDL2main -lSDL2
+	g++ -I.\include -L.\lib -o src/main.exe src/main.cpp -lSDL2main -lSDL2 -lglew32 -lopengl32
 	src/main.exe
 
 clean:
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,56 @@
+#include <GL/glew.h>
 #include <SDL2/SDL.h>
 #include <iostream>
 
-#undef main
+// Define the vertex data
+GLfloat vertices[] = {
+    // positions
+    0.0f,  0.5f, 0.0f,
+   -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f
+};
 
+#undef main
 int main(int argc, char* argv[]) {
+    // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return -1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-    if (!window) {
-        std::cerr << "Failed to create SDL window: " << SDL_GetError() << std::endl;
+    // Set OpenGL version (2.1) and core profile
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("SDL2 and GLEW", 100, 100, 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return -1;
     }
 
+    // Create an OpenGL context
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    if (context == nullptr) {
+        std::cerr << "SDL_GL_CreateContext Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // Initialize GLEW
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        SDL_GL_DeleteContext(context);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // Main loop
     bool running = true;
     SDL_Event event;
     while (running) {
@@ -25,12 +60,29 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        // Rendering code would go here
+        // Render
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        SDL_Delay(10); // Delay to reduce CPU load
+        // Enable client states
+        glEnableClientState(GL_VERTEX_ARRAY);
+
+        // Provide vertex data
+        glVertexPointer(3, GL_FLOAT, 0, vertices);
+
+        // Draw the triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Disable client states
+        glDisableClientState(GL_VERTEX_ARRAY);
+
+        // Swap the buffers
+        SDL_GL_SwapWindow(window);
     }
 
+    // Clean up
+    SDL_GL_DeleteContext(context);
     SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
